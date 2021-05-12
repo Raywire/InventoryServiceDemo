03f62ef24f9bba0b618dfb8a66624892bf4d4b22
Ryan Wire <ryanwire@outlook.com>
2021-05-12 12:41:15 +0300

    Sets up maximum length for fields during user registation

--- InventoryServiceDemo/DTOs/Requests/UserRegisterDto.cs
+++ InventoryServiceDemo/DTOs/Requests/UserRegisterDto.cs
@@ -6,19 +6,24 @@ namespace InventoryServiceDemo.DTOs.Requests
     public class UserRegisterDto
     {
         [Required]
+        [MaxLength(40)]
         public string FirstName { get; set; }
 
         [Required]
+        [MaxLength(40)]
         public string LastName { get; set; }
 
         [Required]
+        [MaxLength(20)]
         public string UserName { get; set; }
 
         [Required]
         [EmailAddress]
+        [MaxLength(64)]
         public string Email { get; set; }
 
         [Required]
+        [MaxLength(32)]
         public string Password { get; set; }
     }
 }
